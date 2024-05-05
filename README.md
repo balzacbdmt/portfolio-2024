ecdf2e1181a0e9bcbc8d54131a6201e5cc1409a0
Balzac <balzac.baudemont@gmail.com>
2024-05-05 18:41:36 +0200

    init project with vite+react

---
+++ README.md
@@ -0,0 +1,13 @@
+# Portfolio 2024
+## Based on React+Vite
+
+## Installation
+
+```
+npm install
+```
+```
+npm run dev
+```
+
+_Developed with ♥️ by Balzac 🥷🏻_
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <h1>Balzac Baudemont</h1>
+      <h2>Portfolio 2024</h2>
+      <h3>(under dev, as u guess it)</h3>
+    </>
+  );
+};
+
+export default App;
