ecade91a664ab42e518fc3b866ed154bc97c2df6
Balzac <balzac.baudemont@gmail.com>
2024-05-05 19:53:38 +0200

    updating hero

--- src/components/features/hero/Hero.tsx
+++ src/components/features/hero/Hero.tsx
@@ -1,10 +1,19 @@
+import styles from "./Hero.module.scss";
+import Avatar from "../../../assets/avatar.png";
+import ScrollIcon from "../../shared/ScrollIcon/ScrollIcon";
+
 const Hero = () => {
   return (
-    <>
-      <h1>Balzac Baudemont</h1>
-      <h2>Portfolio 2024</h2>
-      <h3>(under dev, as u can guess it)</h3>
-    </>
+    <section className={styles.hero}>
+      <div className={styles.title}>
+        <h1>Hi, my name is Balzac.</h1>
+        <h2>
+          I&apos;m a front-end developer based in Paris who loves
+          <br />creating beautiful user interfaces.
+        </h2>
+      </div>
+      <img className={styles.avatar} src={Avatar} alt="" />
+    </section>
   );
 };
 
