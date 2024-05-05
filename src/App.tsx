8d629120865d43b17ca21fd2ce41d8241dba8e34
Balzac <balzac.baudemont@gmail.com>
2024-05-05 19:12:10 +0200

    move content in a hero component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import Hero from "./components/features/hero/Hero";
+
 const App = () => {
   return (
     <>
-      <h1>Balzac Baudemont</h1>
-      <h2>Portfolio 2024</h2>
-      <h3>(under dev, as u guess it)</h3>
+      <Hero />
     </>
   );
 };
---
+++ src/components/features/hero/Hero.tsx
@@ -0,0 +1,11 @@
+const Hero = () => {
+  return (
+    <>
+      <h1>Balzac Baudemont</h1>
+      <h2>Portfolio 2024</h2>
+      <h3>(under dev, as u can guess it)</h3>
+    </>
+  );
+};
+
+export default Hero;
