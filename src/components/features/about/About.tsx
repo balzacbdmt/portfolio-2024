418ccb1eed36df319ba4b1e6f643a3f08c05fc8d
Balzac <balzac.baudemont@gmail.com>
2024-05-09 15:05:23 +0200

    Fix about me section layout

--- src/components/features/about/About.tsx
+++ src/components/features/about/About.tsx
@@ -4,7 +4,16 @@ import styles from "./About.module.scss";
 const About = forwardRef<HTMLElement>((_props, ref) => {
   return (
     <section ref={ref} className={styles.about}>
-      <h1>About me</h1>
+      <div className={styles.leftColumn}>
+        <h1>About&nbsp;<span>me</span></h1>
+        <div className={styles.socials}>
+          <a>Get my resume</a>
+          {/* Github icon */}
+          {/* Linkedin icon */}
+          {/* Instagram icon */}
+          {/* Mail icon */}
+        </div>
+      </div>
       <div className={styles.content}>
         <h2>Relevant experiences</h2>
         {/* TODO Should be a component that import data from a JSON */}
@@ -19,10 +28,11 @@ const About = forwardRef<HTMLElement>((_props, ref) => {
               fermentum nec, malesuada at ante. Fusce dapibus sollicitudin mi,
               eu dapibus risus porta non.
             </p>
+            {/* TODO: Shoud be a shared component */}
             <div className={styles.pillContainer}>
-                <p className={styles.pill}>Nuxt</p>
-                <p className={styles.pill}>Typescript</p>
-                <p className={styles.pill}>D3</p>
+              <p className={styles.pill}>Nuxt</p>
+              <p className={styles.pill}>Typescript</p>
+              <p className={styles.pill}>D3</p>
             </div>
           </div>
         </div>
@@ -38,9 +48,9 @@ const About = forwardRef<HTMLElement>((_props, ref) => {
               eu dapibus risus porta non.
             </p>
             <div className={styles.pillContainer}>
-                <p className={styles.pill}>Nuxt</p>
-                <p className={styles.pill}>Typescript</p>
-                <p className={styles.pill}>D3</p>
+              <p className={styles.pill}>Nuxt</p>
+              <p className={styles.pill}>Typescript</p>
+              <p className={styles.pill}>D3</p>
             </div>
           </div>
         </div>
@@ -56,19 +66,12 @@ const About = forwardRef<HTMLElement>((_props, ref) => {
               eu dapibus risus porta non.
             </p>
             <div className={styles.pillContainer}>
-                <p className={styles.pill}>Nuxt</p>
-                <p className={styles.pill}>Typescript</p>
-                <p className={styles.pill}>D3</p>
+              <p className={styles.pill}>Nuxt</p>
+              <p className={styles.pill}>Typescript</p>
+              <p className={styles.pill}>D3</p>
             </div>
           </div>
         </div>
-      </div>
-      <div className={styles.bottomLeftContainer}>
-        <a>Get my resume</a>
-        {/* Github icon */}
-        {/* Linkedin icon */}
-        {/* Instagram icon */}
-        {/* Mail icon */}
       </div>
     </section>
   );
