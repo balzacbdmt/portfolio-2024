c800daa5d552c47dd41a5be6cea96ca0a266dc49
Balzac <balzac.baudemont@gmail.com>
2024-05-10 21:08:52 +0200

    Update experiences

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -1,13 +1,31 @@
 import { Experience } from "./types";
 
 export const experiences: Experience[] = [
-  {
-    id: 1,
-    date: "Jan 2024 - present",
-    title: "Frontend Engineer",
-    company: "Hopserve",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque feugiat id purus vel tempor. Nullam euismod tellus non magna fermentum placerat. Mauris tellus erat, tempor a fermentum nec, malesuada at ante. Fusce dapibus sollicitudin mi, eu dapibus risus porta non.",
-    skills: ["nuxt", "typescript", "d3"],
-  },
+    {
+      id: 1,
+      date: "Jan 2024 - present",
+      title: "Frontend Engineer",
+      company: "Hopserve",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque feugiat id purus vel tempor. Nullam euismod tellus non magna fermentum placerat. Mauris tellus erat, tempor a fermentum nec, malesuada at ante. Fusce dapibus sollicitudin mi, eu dapibus risus porta non.",
+      skills: ["nuxt", "typescript", "d3"],
+    },
+    {
+      id: 2,
+      date: "Mar 2021 - Dec 2023",
+      title: "Frontend Engineer",
+      company: "Dreem",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque feugiat id purus vel tempor. Nullam euismod tellus non magna fermentum placerat. Mauris tellus erat, tempor a fermentum nec, malesuada at ante. Fusce dapibus sollicitudin mi, eu dapibus risus porta non.",
+      skills: [],
+    },
+    {
+      id: 3,
+      date: "Sep 2019 - Feb 2021",
+      title: "Fullstack Developer",
+      company: "Défipix by Sismik",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque feugiat id purus vel tempor. Nullam euismod tellus non magna fermentum placerat. Mauris tellus erat, tempor a fermentum nec, malesuada at ante. Fusce dapibus sollicitudin mi, eu dapibus risus porta non.",
+      skills: [],
+    },
 ];
