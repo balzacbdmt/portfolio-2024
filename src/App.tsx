d96c0f5bafa1e3da680c848d9d9218baed8ecfe0
Balzac <balzac.baudemont@gmail.com>
2024-05-13 16:21:25 +0200

    remove unused ref

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import { useRef } from "react";
 import About from "./components/features/about/About";
 import Hero from "./components/features/hero/Hero";
 import Cursor from "./components/shared/cursor/Cursor";
 
 const App = () => {
-  const appRef = useRef<HTMLDivElement>(null);
-
   return (
-    <div ref={appRef}>
+    <>
       <Cursor />
       <Hero />
       <About />
-    </div>
+    </>
   );
 };
 
